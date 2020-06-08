a93425a9e4fad551c5e3a4cc021167fc6205a4f9
8-lambda-8 <j.wasleqqq@gmail.com>
2020-06-09 00:01:54 +0200

    implemented Save BrokerName to File

--- app/src/main/java/com/a8lambda8/mqttdashboard/addBrokerActivity.java
+++ app/src/main/java/com/a8lambda8/mqttdashboard/addBrokerActivity.java
@@ -1,16 +1,26 @@
 package com.a8lambda8.mqttdashboard;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.a8lambda8.mqttdashboard.myUtils.LoadStringListFromFile;
+import static com.a8lambda8.mqttdashboard.myUtils.SaveObjectToFile;
+import static com.a8lambda8.mqttdashboard.myUtils.TAG;
 
 public class addBrokerActivity extends AppCompatActivity {
+    EditText ET_Name, ET_Address, ET_Port, ET_uname, ET_passwd, ET_clientId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +33,43 @@ public class addBrokerActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                List<String> brokerNames = LoadStringListFromFile(getString(R.string.key_brokerNames), getBaseContext());
+                if (brokerNames == null){
+                    Log.d(TAG, "brokerNames is null");
+                    brokerNames = new ArrayList<>();
+                }
+
+                if(!(ET_Name.getText().toString().isEmpty()||
+                        ET_Address.getText().toString().isEmpty()||
+                        ET_Port.getText().toString().isEmpty()||
+                        ET_clientId.getText().toString().isEmpty())) {
+                    if(!brokerNames.contains(ET_Name.getText().toString())) {
+
+                        brokerNames.add(ET_Name.getText().toString());
+
+                        SaveObjectToFile(brokerNames,getString(R.string.key_brokerNames),getBaseContext());
+
+                        Snackbar.make(view, "Saved Broker", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                    }else{
+                        Snackbar.make(view, "Broker already exists", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                    }
+
+                }else{
+                    Snackbar.make(view, "Name is null", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
             }
         });
+
+        ET_Name = findViewById(R.id.name);
+        ET_Address = findViewById(R.id.address);
+        ET_Port = findViewById(R.id.port);
+        ET_uname = findViewById(R.id.uname);
+        ET_passwd = findViewById(R.id.passwd);
+        ET_clientId = findViewById(R.id.clientId);
+
     }
 }
---
+++ app/src/main/java/com/a8lambda8/mqttdashboard/myUtils.java
@@ -0,0 +1,62 @@
+package com.a8lambda8.mqttdashboard;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class myUtils {
+    static SharedPreferences SP;
+    static SharedPreferences.Editor SPEdit;
+
+    static String TAG = "xxx";
+
+    List<String> brokerNames;
+
+    //brokerNames
+    static boolean SaveObjectToFile(Object obj, String filename, Context context){
+        boolean worked = true;
+        try {
+            FileOutputStream fos = context.openFileOutput(filename,Context.MODE_PRIVATE);//new FileOutputStream(file);
+            ObjectOutputStream os = new ObjectOutputStream(fos);
+            os.writeObject(obj);
+            os.close();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e(TAG, "SaveObjectToFile exeption: "+e.getMessage());
+            worked = false;
+        }
+        return worked;
+    }
+
+    static Object LoadObjectFromFile(String filename,Context context){
+        Object object = null;
+        try {
+            FileInputStream fis = context.openFileInput(filename);//new FileInputStream(file);
+            ObjectInputStream is = new ObjectInputStream(fis);
+            object = is.readObject();
+            is.close();
+            fis.close();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            Log.e(TAG, "SaveObjectToFile exeption: "+e.getMessage());
+        }
+        return object;
+    }
+
+    static List<String> LoadStringListFromFile(String filename,Context context){
+        ArrayList<String> lst = (ArrayList<String>) LoadObjectFromFile(filename,context);
+        Log.d(TAG, "LoadStringListFromFile: "+lst.toString());
+        return lst;
+    }
+
+}

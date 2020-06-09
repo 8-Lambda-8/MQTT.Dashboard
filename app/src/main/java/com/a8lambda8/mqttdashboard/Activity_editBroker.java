524a0b26438389aed18cc03e765d6a4ed0fab4ef
8-lambda-8 <j.wasleqqq@gmail.com>
2020-06-09 17:13:29 +0200

    Created Activity_editBroker

---
+++ app/src/main/java/com/a8lambda8/mqttdashboard/Activity_editBroker.java
@@ -0,0 +1,64 @@
+package com.a8lambda8.mqttdashboard;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import static com.a8lambda8.mqttdashboard.myUtils.SaveObjectToFile;
+import static com.a8lambda8.mqttdashboard.myUtils.brokerNames;
+
+public class Activity_editBroker extends AppCompatActivity {
+    EditText ET_Name, ET_Address, ET_Port, ET_uname, ET_passwd, ET_clientId;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_broker);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(!(ET_Name.getText().toString().isEmpty()||
+                        ET_Address.getText().toString().isEmpty()||
+                        ET_Port.getText().toString().isEmpty()||
+                        ET_clientId.getText().toString().isEmpty())) {
+                    if(!brokerNames.contains(ET_Name.getText().toString())) {
+
+                        brokerNames.add(ET_Name.getText().toString());
+
+                        SaveObjectToFile(brokerNames,getString(R.string.key_brokerNames),getBaseContext());
+
+                        Snackbar.make(view, "Saved Broker", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                    }else{
+                        Snackbar.make(view, "Broker already exists", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                    }
+
+                }else{
+                    Snackbar.make(view, "Name is null", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            }
+        });
+
+        ET_Name = findViewById(R.id.name);
+        ET_Address = findViewById(R.id.address);
+        ET_Port = findViewById(R.id.port);
+        ET_uname = findViewById(R.id.uname);
+        ET_passwd = findViewById(R.id.passwd);
+        ET_clientId = findViewById(R.id.clientId);
+
+    }
+}

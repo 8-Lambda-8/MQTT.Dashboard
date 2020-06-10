3b11786af96119a8a5ea63fb2ea0aa5fcc59379d
8-lambda-8 <j.wasleqqq@gmail.com>
2020-06-10 11:38:02 +0200

    Created MQTT_Metric Class
    Signed-off-by: 8-lambda-8 <j.wasleqqq@gmail.com>

---
+++ app/src/main/java/com/a8lambda8/mqttdashboard/MQTT_Metric.java
@@ -0,0 +1,67 @@
+package com.a8lambda8.mqttdashboard;
+
+import android.util.Log;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.nio.charset.StandardCharsets;
+
+import static com.a8lambda8.mqttdashboard.myUtils.TAG;
+
+public class MQTT_Metric {
+
+    MqttClient mqttClient;
+
+    String Name;
+    String Topic;
+    boolean publishing;
+    String TopicPub;
+    boolean retained;
+
+    int Qos;
+
+    MQTT_Metric(MqttClient mqttClient) {
+
+        this.mqttClient = mqttClient;
+    }
+
+    void sendMessage(String payload){
+        MqttMessage msg = new MqttMessage();
+        msg.setPayload(payload.getBytes(StandardCharsets.UTF_8));
+        msg.setRetained(retained);
+        msg.setQos(Qos);
+
+        try {
+            mqttClient.publish("/LightSwitch/0/0",msg);
+
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void onClick(){
+
+    }
+
+    void sub(){
+        try {
+            mqttClient.subscribe(Topic, new IMqttMessageListener() {
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    Log.d(TAG, "messageArrived: "+topic+"  "+message);
+                    updateMetric(message);
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updateMetric(MqttMessage message) {
+    }
+
+}
